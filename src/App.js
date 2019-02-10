8fca4268b64aa2f437b4802d58ccd490632fe222
oahtpham <phamthaop.9@gmail.com>
2019-02-10 14:53:51 -0500

    initial fetches

--- src/App.js
+++ src/App.js
@@ -2,25 +2,54 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const STORYURL = ("http://localhost:3000/stories")
+const USERURL = ("http://localhost:3000/users")
+const SPRINTURL = ("http://localhost:3000/sprints")
+const STAGEURL = ("http://localhost:3000/stages")
+const COMMENTURL = ("http://localhost:3000/comments")
+
 class App extends Component {
+
+  state = {
+    users: [],
+    stories: [],
+    sprints: [],
+    stages: [],
+    comments: []
+  }
+
+  componentDidMount() {
+    fetch(STORYURL)
+    .then(r => r.json())
+    .then(storyJson => this.setState({
+      stories: storyJson
+    }))
+    fetch(USERURL)
+    .then(r => r.json())
+    .then(userJson => this.setState({
+      users: userJson
+    }))
+    fetch(SPRINTURL)
+    .then(r => r.json())
+    .then(sprintJson => this.setState({
+      sprints: sprintJson
+    }))
+    fetch(STAGEURL)
+    .then(r => r.json())
+    .then(stageJson => this.setState({
+      stages: stageJson
+    }))
+    fetch(COMMENTURL)
+    .then(r => r.json())
+    .then(commentJson => this.setState({
+      comments: commentJson
+    }))
+  }
+
   render() {
+    console.log(this.state.users)
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      null
     );
   }
 }
