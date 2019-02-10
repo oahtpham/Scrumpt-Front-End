560ff7fed34c88a7d5b953c4457730b51e74ec3f
satyasinha94 <satyasinha94@gmail.com>
2019-02-10 16:24:48 -0500

    containers in container folder

---
+++ src/Components/Sprint.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sprint = ({sprint}) => {
+  return (
+    <div>
+    <p>{sprint.sprint_name}</p>
+    </div>
+  )
+}
+
+export default Sprint
---
+++ src/Components/Stage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Stage = ({stage}) => {
+  return (
+    <div>
+    <p>{stage.stage_name}</p>
+    </div>
+  )
+}
+
+export default Stage
